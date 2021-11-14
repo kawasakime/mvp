31162985088c0e246b3fd6971a1f2a2f067db877
wyylame <w1llhorn@yandex.ru>
2021-11-14 22:53:35 +0300

    fix

--- products/createFiles.py
+++ products/createFiles.py
@@ -7,8 +7,8 @@ for i in range(19):
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/product.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/product.css">
     <title>Roomba 698</title>
 </head>
 <body data-product-id='{i+1}'>
