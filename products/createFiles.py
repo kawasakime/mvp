fa79d80f3d9d1269f005b598483a941ab63f6190
wyylame <w1llhorn@yandex.ru>
2021-11-22 16:22:07 +0300

    add yandex script

--- products/createFiles.py
+++ products/createFiles.py
@@ -11,7 +11,22 @@ for i in range(19):
     <link rel="stylesheet" href="../css/product.css">
     <title>Roomba 698</title>
 </head>
-<body data-product-id='{i+1}'>
+<body data-product-id='{i+1}'\n> """ + """
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(86593878, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/86593878" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->\n
+    """ + """
     <header>
         <div class="wrapper">
             <nav>
@@ -103,6 +118,6 @@ for i in range(19):
     <script src="../cartProductsAmount.js"></script>
 </body>
 </html>
-		""")
+		""");
 	myFile.close();
 	print('OK');
